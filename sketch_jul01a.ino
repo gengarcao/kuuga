95e63263a1880f0d7717b742f76143ddab55c392
gengarcao <925455199@qq.com>
2017-07-03 10:21:20 +0800

    Add files via upload

---
+++ sketch_jul01a.ino
@@ -0,0 +1,30 @@
+int SensorINPUT=3;
+int SensorLED=11;
+unsigned char state=0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(SensorINPUT,INPUT);
+pinMode(SensorLED,OUTPUT);
+attachInterrupt(1,blink,RISING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//Serial.println(valt);
+if(state!=0){
+  state=0;
+ digitalWrite(SensorLED,LOW);
+  Serial.write(1);
+ delay(32);
+}
+
+else{
+  digitalWrite(SensorLED,HIGH);
+}
+}
+
+void blink(){
+  state++;
+}
+
